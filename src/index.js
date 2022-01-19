a8b312d5c43cc06257e30f1c474eccb124576b19
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-19 10:43:46 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 console.log('%c HI', 'color: firebrick')
 
-const imgUrl =  "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
-fetch (imgUrl).then(function(response){
+fetch(imgUrl).then(function (response) {
     return response.json()
-}).then(function (data){
-    console.log(data)
- })
+}).then(function (data) {
+    console.log(data.message)
+    data.message.forEach(element => {
+        console.log(element)
 
- // note
+    });
+})
+
+
+
+// note
+
+//DOM Render Functions
+function renderImage(image) {
+    //Build Image
+    let dog = document.createElement('li')
+}
+
+//Add image to DOM
+//document.querySelector('#dog-image-container').appendChild(dog)
