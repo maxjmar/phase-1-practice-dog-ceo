b9baf19d1103022b73b7cc064cff9bc33075e221
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-01-19 10:08:24 -0500

    first note

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl =  "https://dog.ceo/api/breeds/image/random/4"
+
+fetch (imgUrl).then(function(response){
+    return response.json()
+}).then(function (data){
+    console.log(data)
+ })
+
+ // note
